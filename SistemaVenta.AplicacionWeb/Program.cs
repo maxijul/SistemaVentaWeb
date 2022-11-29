15ea0504626cba208f8a06c11ff85ddb9f3644e4
maxijul <maxijul_14@hotmail.com>
2022-11-29 19:30:39 -0300

    Add Entitys, references y capas, Add Inyeccion de dependencias and class Dependencia

--- SistemaVenta.AplicacionWeb/Program.cs
+++ SistemaVenta.AplicacionWeb/Program.cs
@@ -1,7 +1,13 @@
+using SistemaVenta.IOC;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// A traves del metodo InyectarDependencia se agrega la funcionalidad que creamos en la clase Dependencia
+builder.Services.InyectarDependencia(builder.Configuration);
 
 var app = builder.Build();
 
