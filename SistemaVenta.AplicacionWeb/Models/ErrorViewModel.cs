2bda9e0b20f0618913cc5d4f3326ce7572bb727d
maxijul <maxijul_14@hotmail.com>
2022-11-29 21:24:04 -0300

    Always fixed gitignore

--- SistemaVenta.AplicacionWeb/Models/ErrorViewModel.cs
+++ SistemaVenta.AplicacionWeb/Models/ErrorViewModel.cs
@@ -2,6 +2,10 @@ namespace SistemaVenta.AplicacionWeb.Models
 {
     public class ErrorViewModel
     {
+
+
+
+
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
